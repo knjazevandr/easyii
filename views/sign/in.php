ab13ce12ed509f3df1b15fe60cf76f76363e8ed3
Князев Андрей <knjazevandr@gmail.com>
2015-04-23 10:48:16 +0600

    Изменение формы авторизации

--- views/sign/in.php
+++ views/sign/in.php
@@ -25,11 +25,11 @@ $this->title = Yii::t('easyii', 'Sign in');
                     <?php ActiveForm::end(); ?>
                 </div>
             </div>
-            <div class="text-center">
-                <a class="logo" href="http://easyiicms.com" target="_blank" title="EasyiiCMS homepage">
-                    <img src="<?= $asset->baseUrl ?>/img/logo_20.png">EasyiiCMS
-                </a>
-            </div>
+<!--            <div class="text-center">-->
+<!--                <a class="logo" href="http://easyiicms.com" target="_blank" title="EasyiiCMS homepage">-->
+<!--                    <img src="--><?//= $asset->baseUrl ?><!--/img/logo_20.png">EasyiiCMS-->
+<!--                </a>-->
+<!--            </div>-->
         </div>
     </div>
 </div>
